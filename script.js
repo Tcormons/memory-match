b71568e7fb088a6c683851cd7ff5de8acb559498
Tomas Cormons <tcormons@gmail.com>
2019-10-02 15:59:44 -0700

    check for card match, updated banner design

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 $(document).ready(intializApp);
 
-function intializApp(){
-  $('.card').on('click',handleCardClick);
-  $('.card').on('click',assignClicks);
+function intializApp() {
+  $('.card').on('click', handleCardClick);
+  $('.card').on('click', cardFlip);
 }
 
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
+var firstCardCompare = null;
+var secondCardCompare = null;
 
-function handleCardClick(event){
+function handleCardClick(event) {
   $(event.currentTarget).find('.back-card').addClass('hidden');
 }
 
-
-
-function assignClicks(event){
+function cardFlip(event) {
   // debugger;
   if (firstCardClicked === null) {
     firstCardClicked = $(event.currentTarget);
-    var firstCardCompare = firstCardClicked.find('.front').css('background-image');
-    console.log(firstCardCompare);
-
+    firstCardCompare = firstCardClicked.find('.front').css('background-image');
+    return;
   } else if (firstCardClicked !== null) {
     secondCardClicked = $(event.currentTarget);
-    var secondCardCompare = secondCardClicked.find('.front').css('background-image');
-    console.log(secondCardCompare);
+    secondCardCompare = secondCardClicked.find('.front').css('background-image');
   }
 
-
-
-
-  if (firstCardCompare == secondCardCompare){
-    matches ++;
-    console.log(matches);
-
+  if (secondCardCompare == null) {
+    console.log("Pick a second card");
+  } else if (firstCardCompare === secondCardCompare) {
+    matches++;
+    console.log("You have a match");
+    firstCardClicked = null;
+    secondCardClicked = null;
   } else {
-    // setTimeout(misMatch(), 1500);
+    setTimeout(misMatch, 1500);
   }
-
 }
 
-function misMatch(){
-  firstCardClicked.addClass('hidden');
-  secondCardClicked.addClass('hidden');
-
+function misMatch() {
+  // debugger;
+  firstCardClicked.find('.back-card').removeClass('hidden');
+  secondCardClicked.find('.back-card').removeClass('hidden');
+  firstCardClicked = null;
+  secondCardClicked = null;
 }
