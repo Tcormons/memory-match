30030d62933f8ddfd87e68ada2ad238a6f307f79
Tomas Cormons <tcormons@gmail.com>
2019-10-03 16:25:22 -0700

    working on conditions to calculate values in game, slight css adjustments

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ var firstCardClicked = null;
 var secondCardClicked = null;
 var firstCardCompare = null;
 var secondCardCompare = null;
-var matches = null;
+var matches = 0;
 var maxMatches = 9;
+var attempts = 0;
+var gamesPlayed = 0;
 
 function handleCardClick(event) {
   $(event.currentTarget).find('.back-card').addClass('hidden');
@@ -21,40 +23,84 @@ function cardFlip(event) {
     firstCardClicked = $(event.currentTarget);
     firstCardCompare = firstCardClicked.find('.front').css('background-image');
     return;
+
   } else if (firstCardClicked !== null) {
     secondCardClicked = $(event.currentTarget);
     secondCardCompare = secondCardClicked.find('.front').css('background-image');
+
+    // check for double click on same firstCard
+    if (firstCardClicked.is(secondCardClicked)){
+      secondCardClicked = null;
+      console.log('You need to pick a second card');
+      return;
+    }
   }
 
-  if (secondCardCompare == null) {
-    console.log("Pick a second card");
-  } else if (firstCardCompare === secondCardCompare) {
+  // Match
+  if (firstCardCompare === secondCardCompare) {
     console.log("You have a match");
-    setTimeout(completedPair, 300);
-  } else {
     $('.card').unbind('click');
-    setTimeout(misMatch, 500);
+    setTimeout(removeCompletedPair, 300);
+    attempts ++;
+    displayStats();
+  }
+  // Mismatch
+  else {
+    console.log('Try again');
+    $('.card').unbind('click');
+    setTimeout(handleMisMatch, 500);
+    attempts ++;
+    displayStats();
   }
 }
 
-function completedPair(){
-  matches++;
-
+function removeCompletedPair(){
   firstCardClicked.addClass('disappear');
   secondCardClicked.addClass('disappear');
   firstCardClicked = null;
   secondCardClicked = null;
+  matches++;
+
+  // Re-apply click handlers
+  $('.card').on('click', handleCardClick);
+  $('.card').on('click', cardFlip);
+
+  // completed game condition
   if (matches === maxMatches) {
     var modal = $('<div>').addClass('modal');
     $('.container').prepend(modal);
+    gamesPlayed ++;
+    displayStats();
   }
 }
-function misMatch() {
+
+function handleMisMatch() {
   firstCardClicked.find('.back-card').removeClass('hidden');
   secondCardClicked.find('.back-card').removeClass('hidden');
   firstCardClicked = null;
   secondCardClicked = null;
+
   // Re-apply click handlers
   $('.card').on('click', handleCardClick);
   $('.card').on('click', cardFlip);
+}
+
+function calculateAccuracy(){
+  var calcAccuracy = matches/attempts;
+  return calcAccuracy;
+}
+
+function displayStats(){
+  // debugger;
+
+  var accuracy = calculateAccuracy();
+
+  var gameDiv = $('#gamesPlayed');
+  var attemptDiv = $('#attempts');
+  var accuracyDiv = $('#accuracy');
+
+  gameDiv.text(gamesPlayed);
+  attemptDiv.text(attempts);
+  accuracyDiv.text(accuracy);
+
 }
